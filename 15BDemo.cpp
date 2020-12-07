c774d373ba5e95736bfbba8b12bb143af3ab528a
Patricia Cavendish <patricia.cavendish@sinclair.edu>
2020-12-06 21:48:39 -0500

    Add project files.

---
+++ 15BDemo.cpp
@@ -0,0 +1,22 @@
+/*Patricia Cavendish
+*CIS 1202 502
+*Due 12/06/2020
+*12/06/2020
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+	cout << endl << endl;
+	system("PAUSE");
+	return 0;
+}
